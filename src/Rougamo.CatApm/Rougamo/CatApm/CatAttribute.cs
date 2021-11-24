5c1622ecb8d693c0828e61cdecaf195ea1487cdd
ihourglass <inversionhourglass@outlook.com>
2021-11-25 00:40:53 +0800

    searching for best implement of cat client in .net core

---
+++ src/Rougamo.CatApm/Rougamo/CatApm/CatAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rougamo.CatApm
+{
+    /// <summary>
+    /// cat transaction annotation
+    /// </summary>
+    public class CatAttribute : MoAttribute
+    {
+    }
+}
